546f343c5f44433a3e918fea175d94d7e1d043fd
bulabd <bulabd@outlook.com>
2022-04-04 19:31:29 +0000

    Recreated the console.assert() method

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,12 @@
 // FUNCTION IMPLEMENTATION
-const sum = function(a, b) { 
-  return a + b;
-}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✔️ ✔️ ✔️ Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
 
 // TEST CODE
-console.assert(sum(1, 2) === 3);
-console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
