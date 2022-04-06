28fd88fd05f257d629d44e96f7df091758a0831a
bulabd <bulabd@outlook.com>
2022-04-06 20:20:54 +0000

    Fixed eqArray issue

--- eqObjects.js
+++ eqObjects.js
@@ -10,12 +10,16 @@ const assertEqual = function(actual, expected) {
 // Otherwise you get back a big fat false!
 const eqArrays = function(actual, expected) {
   let truthCounter = 0;
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] === expected[i]) {
-      truthCounter++;
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] === expected[i]) {
+        truthCounter++;
+      }
     }
   }
-  if (truthCounter === actual.length) {
+  if (truthCounter === actual.length && truthCounter === expected.length) {
     return true;
   }
   return false;
@@ -26,31 +30,32 @@ const eqObjects = function(object1, object2) {
   let keyArr2 = Object.keys(object2);
   if (keyArr1.length !== keyArr2.length) {
     return false;
-  } else {
-    for (let key of keyArr1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
-      } else {
-        if (object1[key] !== object2[key]) {
-          return false;
-        }
+  }
+  for (let key of keyArr1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
-    return true;
   }
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(eqObjects(ab, ba)); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
 console.log(eqObjects(ab, abc)); // => false
-
 assertEqual(eqObjects(ab, ba), true);
+
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+console.log(eqObjects(cd, cd2)); // => false
+console.log(eqArrays(cd["d"], cd2["d"])); // => false
--- letterPositions.js
+++ letterPositions.js
@@ -1,11 +1,15 @@
 const eqArrays = function(actual, expected) {
   let truthCounter = 0;
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] === expected[i]) {
-      truthCounter++;
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] === expected[i]) {
+        truthCounter++;
+      }
     }
   }
-  if (truthCounter === actual.length) {
+  if (truthCounter === actual.length && truthCounter === expected.length) {
     return true;
   }
   return false;
