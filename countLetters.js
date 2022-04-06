80a43716bd77080096f7826898ec8278041429f9
bulabd <bulabd@outlook.com>
2022-04-06 17:35:05 +0000

    Implement countLetters solution

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let noSpaces = str.split(" ").join('');
+  let result = {};
+  for (let letter of noSpaces) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
