174fc3b15a1f5a205f4a26871f58c85b0423c3d3
bulabd <bulabd@outlook.com>
2022-04-06 00:22:58 +0000

    Implement flatten

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(actual, expected) {
+  let truthCounter = 0;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] === expected[i]) {
+      truthCounter++;
+    }
+  }
+  if (truthCounter === actual.length) {
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else if (!(eqArrays(actual, expected))) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+  let newArr = [];
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      const result = flatten(element);
+      result.forEach(element => newArr.push(element));
+    } else {
+      newArr.push(element);
+    }
+  }
+  return newArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1,2,3,4,5,6]);
