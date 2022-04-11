a4ad936aecdf1ffbb180b9ba45b2250e8cb76fe8
bulabd <bulabd@outlook.com>
2022-04-11 12:19:44 +0000

    Refactored the head and assertEqual

--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) { 
-  if (actual === expected) { 
-    console.log(`✔️ ✔️ ✔️ Assertion Passed: ${actual} === ${expected}`); 
-  } else if (actual !== expected) { 
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`); 
-  } 
-};
-
 const head = function(arr) {
   return arr[0];
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), 6);
+module.exports = head;
