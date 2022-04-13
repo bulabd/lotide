1192c1f9bc3b3ed332aea84dc94e3851798f3eb1
bulabd <bulabd@outlook.com>
2022-04-13 13:29:53 +0000

    Refactored so that the function can compare nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -3,8 +3,14 @@ const eqArrays = (actual, expected) => {
     return false;
   }
   for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
+    if (Array.isArray(actual[i]) && Array.isArray(expected[i])) {
+      if (!eqArrays(actual[i], expected[i])) {
+        return false;
+      }
+    } else {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
     }
   }
   return true;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -15,4 +15,16 @@ describe('#eqArrays', () => {
     assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
   });
 
+  it('returns true for [[2, 3], [4]] and [[2, 3], [4]]', () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+
+  it('returns false for [[2, 3], [4]] and [[2, 3], [4, 5]', () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+
+  it('returns false for [[2, 3], [4]] and [[2, 3], 4]', () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+
 });
